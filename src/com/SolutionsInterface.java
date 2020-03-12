98d7e5f29f4ae08340b3e004fd7f585dc083e1bb
majorMobilych <godmihovelcplus@gmail.com>
2020-03-12 20:09:16 +0300

    Fix test class

--- src/com/SolutionsInterface.java
+++ src/com/SolutionsInterface.java
@@ -5,7 +5,7 @@ public interface SolutionsInterface {
      * @param firstArray - one of two given sorted arrays
      * @param secondArray - another given sorted array
      * @return new array, merged of two ones above
-     * @see SolutionsInterfaceImpl#merge(int[], int[]) for full description of algoritm
+     * @see SolutionsInterfaceImpl#merge(int[], int[]) for full description of algorithm
      */
     int[] merge(int[] firstArray, int[] secondArray);
 
@@ -13,8 +13,8 @@ public interface SolutionsInterface {
      * @param firstArray - one of two given sorted arrays
      * @param secondArray - another given sorted array
      * @return new array, merged of two ones above
-     * @see SolutionsInterfaceImpl#mergeUsingIntStreams(int[], int[]) for full description of algoritm
-     * less writing
+     * @see SolutionsInterfaceImpl#mergeUsingIntStreams(int[], int[]) for full description of algorithm
+     * less writing, used in tests
      */
     int[] mergeUsingIntStreams(int[] firstArray, int[] secondArray);
 }
